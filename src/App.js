cf05ad51708aaa8acb95b47067d934a34d4eae15
Maryam Verdon <maryamverdon@gmail.com>
2022-10-03 16:44:16 +0200

    finished style + add search bar

--- src/App.js
+++ src/App.js
@@ -13,7 +13,17 @@ const App = () => {
 
   return (
     <div className="bg-white">
-      <h2 className="m-10">Recettes</h2>
+      <h2 className="my-10 text-4xl text-center text-red-400 font-bold">
+        React Cook
+      </h2>
+      <div class="flex flex-col justify-center items-center">
+        <input
+          type="text"
+          placeholder="Type the name of a food"
+          className="form-control relative flex-auto w-80 min-w-0 block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-red-400 focus:outline-none"
+        />
+      </div>
+
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {receipts.map((receipt, index) => (
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Card = ({receipt}) => {
+   
     return (
         <div className="group">
             <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
@@ -8,7 +9,7 @@ const Card = ({receipt}) => {
             </div>
             <p className="mt-1 text-lg font-medium text-gray-900">{receipt.strMeal}</p>
             <h3 className="mt-4 text-sm text-gray-700">{receipt.strArea}</h3>
-            <p class="mt-1 text-sm text-gray-500">Mes ingrédients....</p>
+            <p className="line-clamp-5 mt-1 text-sm text-gray-500">{receipt.strInstructions}</p>
 
         </div>
         
